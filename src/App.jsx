939f781c8fcf469d1330cb1c58c04efeb9a78f85
jagadish011 <munavallijagadish@gmail.com>
2024-07-20 11:05:06 +0530

    admin changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Sidebar from './components/Sidebar';
@@ -14,7 +14,6 @@ import AllBooking from './pages/AllBookings';
 import BookingDatePage from './pages/BookingDatePage';
 import { AuthProvider } from './context/AuthProvider';
 
-
 function App() {
   return (
     <div className='h-full'>
@@ -22,16 +21,17 @@ function App() {
         <Router>
           <Routes>
             <Route path="/login" element={<Login />} />
-            <Route path="" element={<Sidebar />}>
-              <Route path='home' element={<Home/>}/>
+            <Route path="/" element={<Navigate to="/login" />} />
+            <Route path="/" element={<Sidebar />}>
+              <Route path="home" element={<Home />} />
               <Route path="allCustomer" element={<AdminCustomer />} />
-              <Route path="/customerDetails/:id" element={<CustomerDetail />} />
-              <Route path= "/addCustomer" element={<AddCustomer />} />
-              <Route path= "/editCustomer/:id" element={<EditCustomer />} />
-              <Route path= "/customerBooking/:id" element={<CustomerBooking />} />
-              <Route path= "/allBookings" element={<AllBooking />} />
-              <Route path='/bookings' element={<BookingDatePage/>}/>
-              <Route path='/profile' element={<Profile/>}/>
+              <Route path="customerDetails/:id" element={<CustomerDetail />} />
+              <Route path="addCustomer" element={<AddCustomer />} />
+              <Route path="editCustomer/:id" element={<EditCustomer />} />
+              <Route path="customerBooking/:id" element={<CustomerBooking />} />
+              <Route path="allBookings" element={<AllBooking />} />
+              <Route path="bookings" element={<BookingDatePage />} />
+              <Route path="profile" element={<Profile />} />
             </Route>
           </Routes>
         </Router>
